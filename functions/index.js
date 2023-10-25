e61aa622569870fc78c5dd053a7c9e644443537e
mmaismma <mmaismma@hotmail.com>
2023-10-25 21:43:31 +0530

    Updat blocking function to output detailed results

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,10 @@
-import {HttpsError} from "firebase-functions/v2/identity";
-import {beforeUserCreated} from "firebase-functions/v2/identity";
+// TODO: convert to ESM
 
-export const sanitizeUserCreation = beforeUserCreated((e) => {
+const {HttpsError} = require("firebase-functions/v2/identity");
+const {beforeUserCreated} = require("firebase-functions/v2/identity");
+const {onRequest} = require("firebase-functions/v2/https");
+
+module.exports.monitorUserCreation = beforeUserCreated((e) => {
   const [entryNo, provider] = e.data.email.split("@");
 
   // Reject all non-organisational email ids
@@ -19,7 +22,7 @@ export const sanitizeUserCreation = beforeUserCreated((e) => {
       "i",
   );
 
-  const [, year, degree, branch, rollNo] = entryNoRegex.exec(entryNo) || [];
+  let [, year, degree, branch, rollNo] = entryNoRegex.exec(entryNo) || [];
 
   if (!year || !degree || !branch || !rollNo) {
     throw new HttpsError("permission-denied",
@@ -27,14 +30,45 @@ export const sanitizeUserCreation = beforeUserCreated((e) => {
         Entry number: ${year}, ${degree}, ${branch}, ${rollNo}.`);
   }
 
+  year = parseInt(year) + 2000;
+
+  const branchMap = {
+    cs: "Computer Science and Engineering",
+    ec: "Electronics and Communications Engineering",
+    ee: "Electrical Engineering",
+    me: "Mechanical Engineering",
+    ce: "Civil Engineering",
+  };
+
+  const degreeMap = {
+    b: "Bachelor",
+  };
+
+  const graduationDuration = {
+    bcs: 4,
+  };
+
   return {
     // Add user info
+    displayName: e.data.displayName.replaceAll(entryNo, "").trim(),
     customClaims: {
-      year,
-      degree,
-      branch,
-      rollNo,
-      entryNo,
+      studentInfo: {
+        year,
+        degree: degreeMap[degree],
+        branch: branchMap[branch],
+        rollNo: parseInt(rollNo),
+        graduationDuration: year + graduationDuration[degree + branch],
+        entryNo,
+      },
     },
   };
+});
+
+// Server-side rendering
+let ssrServer;
+module.exports.ssr = onRequest(async (request, response) => {
+  if (!ssrServer) {
+    ssrServer = require("./ssr/index").default;
+  }
+  return ssrServer(request, response);
 });
